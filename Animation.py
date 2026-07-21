e759fcaf748322a08b65c1a291007a511dc234b6
Nachiket <nachi7917@gmail.com>
2026-07-21 18:53:03 +1000

    Added Colours

--- Animation.py
+++ Animation.py
@@ -3,15 +3,28 @@ from manim import *
 class EMS(Scene):
     def construct(self):
 
-        sun = Circle(radius=3)
+        #Creates the Celestial bodies
+        
+        sun = Circle(radius=1)
         earth = Circle(radius=0.5)
         moon = Circle(radius=0.1)
-
+        
+        #Gives them colour,opacity and set their border width to zero
+        
+        sun.set_fill(YELLOW, opacity=1).set_stroke(width=0)
+        earth.set_fill(DARK_BLUE, opacity=1).set_stroke(width=0)
+        moon.set_fill(GREY, opacity=1).set_stroke(width=0)
+        
+        #Adds the text
+        
         text = Text("Earth Moon Sun Animation")
+
+        #Plays the animation
 
         self.wait(2)
 
         self.play(Create(sun))
+
         self.play(Create(earth))
         self.play(Create(moon))
 
