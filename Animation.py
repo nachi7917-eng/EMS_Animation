df50ca6dd4d3dfba3ef53917dac775f72ff8764d
Nachiket <nachi7917@gmail.com>
2026-07-21 18:42:40 +1000

    Added Bodies and text

--- Animation.py
+++ Animation.py
@@ -2,6 +2,21 @@ from manim import *
 
 class EMS(Scene):
     def construct(self):
+
+        sun = Circle(radius=3)
+        earth = Circle(radius=0.5)
+        moon = Circle(radius=0.1)
+
         text = Text("Earth Moon Sun Animation")
+
+        self.wait(2)
+
+        self.play(Create(sun))
+        self.play(Create(earth))
+        self.play(Create(moon))
+
+        self.wait(2)
+
         self.play(Write(text))
+
         self.wait(2)
