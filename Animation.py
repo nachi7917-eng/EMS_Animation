d7bfe5d055439b9b1a811b36e17bf160753c77c8
Nachiket <nachi7917@gmail.com>
2026-07-21 19:48:50 +1000

    Added Value Tracker

--- Animation.py
+++ Animation.py
@@ -1,62 +1,3 @@
-'''from manim import *
-import numpy as np
-
-class EMS(Scene):
-    def construct(self):
-        # Create celestial bodies
-        sun = Circle(radius=0.5).set_fill(YELLOW, opacity=1).set_stroke(width=0)
-        earth = Circle(radius=0.2).set_fill(DARK_BLUE, opacity=1).set_stroke(width=0)
-        moon = Circle(radius=0.05).set_fill(GREY, opacity=1).set_stroke(width=0)
-
-        # Add objects
-        self.play(FadeIn(sun))
-        self.play(FadeIn(earth))
-        self.play(FadeIn(moon))
-        self.wait(1)
-
-        # Move into position
-        self.play(
-            sun.animate.move_to(ORIGIN),
-            earth.animate.move_to(RIGHT * 3),
-            moon.animate.move_to(RIGHT * 3.5),
-            run_time=3
-        )
-
-        # Angle tracker drives both orbits
-        t = ValueTracker(0)
-
-        earth_orbit_radius = 3
-        moon_orbit_radius = 0.5
-        moon_speed = 4  # moon orbits 4x per earth year (real value is ~13)
-
-        earth.add_updater(lambda m: m.move_to(
-            sun.get_center() + earth_orbit_radius * np.array([
-                np.cos(t.get_value()), np.sin(t.get_value()), 0
-            ])
-        ))
-
-        # Moon orbits wherever the earth currently is
-        moon.add_updater(lambda m: m.move_to(
-            earth.get_center() + moon_orbit_radius * np.array([
-                np.cos(moon_speed * t.get_value()),
-                np.sin(moon_speed * t.get_value()), 0
-            ])
-        ))
-
-        # Animate one full earth orbit
-        self.play(t.animate.set_value(TAU), run_time=10, rate_func=linear)
-        #updater cleaner
-        earth.clear_updaters()
-        moon.clear_updaters()
-        
-        self.wait(1)
-
-        # Title Animation
-        text = Text("Earth-Moon-Sun Animation")
-        text.move_to(DOWN * 2)
-        self.play(Write(text))
-        self.wait(2)'''
-
 from manim import *
 
 class EMS(Scene):
@@ -94,7 +35,10 @@ class EMS(Scene):
             moon.animate.move_to(moon_target),
             run_time=3
         )
-        moon.add_updater(lambda m: m.move_to(earth.get_center() + RIGHT))
+        t = ValueTracker(0)
+        moon.add_updater(lambda m: m.move_to(
+            earth.get_center() + np.array([np.cos(t.get_value()), np.sin(t.get_value()), 0])
+            ))
         self.play(Rotate(earth, angle=TAU, about_point=sun.get_center(), run_time=10))
         moon.clear_updaters()
 
